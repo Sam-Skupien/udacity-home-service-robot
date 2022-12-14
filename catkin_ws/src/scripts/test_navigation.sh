a45b3088da41a8e4194524558585c7ca4315b8a4
Sam-Skupien <s.skupien1@gmail.com>
2022-12-14 03:51:29 +0000

    Updated map files.

--- catkin_ws/src/scripts/test_navigation.sh
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-xterm -e "export ROBOT_INITIAL_POSE='-x 2 -y 2 -z 0 -R 0 -P 0 -Y 0';
-roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world;" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world;" &
 sleep 15
 
-xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/maps/map.yaml" &
 sleep 5
 
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 5
 
-xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
 
--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-xterm -e "export ROBOT_INITIAL_POSE='-x 2 -y 2 -z 0 -R 0 -P 0 -Y 0';
-roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world;" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world;" &
 sleep 15
 
 xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
