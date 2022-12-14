c161750a7b364d86b93a421ca93a1c52b5e42714
Sam-Skupien <s.skupien1@gmail.com>
2022-12-14 02:50:26 +0000

    Fixed world file.

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e "export ROBOT_INITIAL_POSE='-x -0 -y -0 -z 0 -R 0 -P 0 -Y 0';
+xterm -e "export ROBOT_INITIAL_POSE='-x 2 -y 2 -z 0 -R 0 -P 0 -Y 0';
 roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world;" &
 sleep 15
 
