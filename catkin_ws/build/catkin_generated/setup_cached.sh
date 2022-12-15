ef0a724c07908b93e0cf4676f25f74e5c9c62b79
Sam-Skupien <s.skupien1@gmail.com>
2022-12-15 03:22:51 +0000

    Started work on pick objects section.

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/workspace/catkin_ws/devel:/opt/ros/kinetic"
-export LD_LIBRARY_PATH="/home/workspace/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-9.0/lib64:/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/local/lib/x86_64-linux-gnu:/usr/local/lib/i386-linux-gnu:/usr/lib/x86_64-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/nvidia/lib:/usr/local/nvidia/lib64"
-export PKG_CONFIG_PATH="/home/workspace/catkin_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/workspace/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/workspace/catkin_ws/src:/opt/ros/kinetic/share"
---
+++ catkin_ws/src/scripts/pick_objects.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world;" &
+sleep 15
+
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/maps/map.yaml initial_pose_x:=30 initial_pose_y:=30 initial_pose_a:=-1.57" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+xterm -e "rosrun pick_objects pick_objects"
+
+
