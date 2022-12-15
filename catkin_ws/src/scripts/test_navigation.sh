39e531290c155173a4d470a0377127ad837c42e3
Sam-Skupien <s.skupien1@gmail.com>
2022-12-15 02:34:48 +0000

    Finished test navigation script.

--- catkin_ws/src/scripts/test_navigation.sh
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -2,7 +2,7 @@
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world;" &
 sleep 15
 
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/maps/map.yaml initial_pose_x:=-4 initial_pose_y:=-23 initial_pose_a:=0" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/maps/map.yaml initial_pose_x:=30 initial_pose_y:=30 initial_pose_a:=-1.57" &
 sleep 5
 
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
