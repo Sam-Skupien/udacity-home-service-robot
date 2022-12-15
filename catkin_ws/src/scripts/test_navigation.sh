db765362461b51aa572062bdfb43e3ec88ee0396
Sam-Skupien <s.skupien1@gmail.com>
2022-12-15 01:51:28 +0000

    added amcl pose.

--- catkin_ws/src/scripts/test_navigation.sh
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -2,7 +2,7 @@
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/myworld.world;" &
 sleep 15
 
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/maps/map.yaml" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/maps/map.yaml initial_pose_x:=-4 initial_pose_y:=-23 initial_pose_a:=0" &
 sleep 5
 
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
